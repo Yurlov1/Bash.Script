1a31b9896c592d2ccd9e5efa9c268b85ab1b7238
Dmitriy <Dima.yurlov@gmail.com>
2021-10-12 17:19:40 +0200

    Script

---
+++ script1.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+ls
+pwd
+whoami 
+mkdir Folder1
+touch text1.txt
+cp -p text1.txt text2.txt
+cd Folder1
+touch text3.txt
+cp -p text3.txt text4.txt
+mkdir Folder2
+mv text4.txt Folder2
+cd ..
+cd ..
